c6197b5176a8cdc6a7bb78b9e65c5155b85a0fb1
gustavwogen <gustavwogen@gmail.com>
2023-05-11 20:40:17 -0400

    yup

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import streamlit as st
 
-from spotify import create_client, create_tracks_df
+from spotify import create_client, create_top_tracks_df, create_top_artists_df
 
 
 def main():
+    st.title("Spotify Dashboard")
+    col1, col2 = st.columns(2)
+    button_clicked = "songs"
+    with col1:
+        song_button = st.button("Top Songs")
+    with col2:
+        artist_button = st.button("Top Artists")
+
     client = create_client()
-    df = create_tracks_df(client)
-    songs, artists, albums = df['track title'], ['artists'], df['album']
+    if song_button:
+        button_clicked = "songs"
+        df = create_top_tracks_df(client)
+        df
+    elif artist_button:
+        button_clicked = "artists"
+        df = create_top_artists_df(client)
+        df
+
+    print("button clicked:", button_clicked)
     # st.set_page_config(
     #     page_title="Spotify Dashboard",
     #     page_icon="🧊",
@@ -20,13 +35,10 @@ def main():
     #         'About': "# This is a header. This is an *extremely* cool app!"
     #     }
     # )
-    st.title('Spotify Dashboard')
-    col1, col2, col3 = st.columns(3)
     # st.table(data=df)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -1,24 +1,33 @@
 from unittest.mock import Mock, patch
+import json
 
-import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import spotify
 
 
 @patch("spotify.SpotifyOAuth")
 @patch("spotify.spotipy")
 def test_create_client(mock_spotipy, mock_oauth):
-    mock_spotipy.Spotify.return_value = spotipy.client.Spotify
-    mock_oauth.return_value = spotipy.oauth2.SpotifyOAuth
+    scope = "user-top-read"
+    mock_spotipy.Spotify.return_value = Mock(name="client")
+    mock_oauth.return_value = Mock(name="SpotifyOAuth")
 
     spotify.create_client()
 
+    mock_oauth.assert_called_once_with(scope=scope)
     mock_spotipy.Spotify.assert_called_once_with(auth_manager=mock_oauth())
 
 
-
-def test_create_tracks_df():
-    keys_in_res = ['items', 'total', 'limit', 'offset', 'href', 'next', 'previous']
+@patch("spotify.pd")
+def test_create_top_tracks_df(mock_pd):
     mock_client = Mock(name="client")
-    mock_client.current_user_top_tracks.keys.return_value = keys_in_res
-    
-    
+    LIMIT = 50
+    with open("tests/responses/tracks.json") as f:
+        sample_response = json.load(f)
+
+    mock_client.current_user_top_tracks.return_value = sample_response
+
+    mock_df = Mock(name="mock_dataframe")
+    mock_pd.DataFrame.from_records.return_value = mock_df
+
+    spotify.create_top_tracks_df(mock_client)
